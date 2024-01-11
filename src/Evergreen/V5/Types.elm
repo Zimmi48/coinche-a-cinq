74f42344462d886be7c8613503b9e1be40db4d01
Théo Zimmermann <theo.zimmermann@telecom-paris.fr>
2024-01-11 20:01:51 +0100

    Autogenerated migration.

---
+++ src/Evergreen/V5/Types.elm
@@ -0,0 +1,106 @@
+module Evergreen.V5.Types exposing (..)
+
+import Browser
+import Browser.Navigation
+import Dict
+import Lamdera
+import Url
+
+
+type Suit
+    = Clubs
+    | Diamonds
+    | Hearts
+    | Spades
+
+
+type Rank
+    = Ace
+    | Five
+    | Six
+    | Seven
+    | Eight
+    | Nine
+    | Ten
+    | Jack
+    | Queen
+    | King
+
+
+type alias Card =
+    { suit : Suit
+    , rank : Rank
+    }
+
+
+type alias FrontendModel =
+    { key : Browser.Navigation.Key
+    , name : String
+    , playing : Bool
+    , hand : List Card
+    , played : Dict.Dict String Card
+    , playerLeft : Maybe String
+    , playerRight : Maybe String
+    , playerTopLeft : Maybe String
+    , playerTopRight : Maybe String
+    , trump : Maybe Suit
+    , scores : Dict.Dict String Int
+    }
+
+
+type alias Player =
+    { name : String
+    , id : Lamdera.SessionId
+    }
+
+
+type alias Game =
+    { hands : Dict.Dict Lamdera.SessionId (List Card)
+    , gathered : Dict.Dict Lamdera.SessionId (List Card)
+    , played : Dict.Dict Lamdera.SessionId Card
+    , trump : Maybe Suit
+    }
+
+
+type alias BackendModel =
+    { players : List Player
+    , game : Maybe Game
+    }
+
+
+type FrontendMsg
+    = UrlClicked Browser.UrlRequest
+    | UrlChanged Url.Url
+    | UpdateName String
+    | StartGame
+    | PlayCard Card
+    | GatherCards
+    | ChangeTrump Suit
+    | NoOpFrontendMsg
+
+
+type ToBackend
+    = NoOpToBackend
+    | JoinGame String
+    | Reset
+    | Played Card
+    | Gathered
+    | TrumpChanged Suit
+    | RestoreName
+    | RestoreSession
+
+
+type BackendMsg
+    = NoOpBackendMsg
+    | NewGame Game
+
+
+type ToFrontend
+    = NoOpToFrontend
+    | PlayersList (List String)
+    | GiveHand (List Card)
+    | PlayedBy String Card
+    | ClearPlayed
+    | NewTrump Suit
+    | RestoredName String
+    | Scores (Dict.Dict String Int)
